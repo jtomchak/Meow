bd5b36385af7e447b1e636d3071319d5b7753207
Jesse Tomchak <jtomchak@gmail.com>
2018-01-09 20:18:32 -0700

    base token and lex test

---
+++ README.md
@@ -0,0 +1,3 @@
+# Meow
+
+* Meow is a simple programming language based of Monkey, a language build by Thorsten Ball for the purpose of building an interpreter in Go. I followed his book [Writing an Interpreter in Go](https://interpreterbook.com/)to be Meow up and running, and learn about interpreters and compilers in the process. Enjoy!
---
+++ lexer/lexer_test.go
@@ -0,0 +1,41 @@
+package lexer
+
+import (
+	"testing"
+
+	"meow/token"
+)
+
+func TestNextToken(t *testing.T) {
+	input := `=+(){},;`
+
+	tests := []struct {
+		expectedType   token.TokenType
+		expctedLiteral string
+	}{
+		{token.ASSIGN, "="},
+		{token.PLUS, "="},
+		{token.LPAREN, "("},
+		{token.RPAREN, ")"},
+		{token.LBRACE, "{"},
+		{token.RBRACE, "}"},
+		{token.COMMA, ","},
+		{token.SEMICOLON, ";"},
+		{token.EOF, ""},
+	}
+
+	l := New(input)
+
+	for i, tt := range tests {
+		tok := l.NextToken()
+
+		if tok.Type != tt.expectedType {
+			t.Fatalf("tests[%d] - tokentype wrong. exprected=%q, got=%q", i, tt.expectedType, tok.Type)
+		}
+
+		if tok.Literal != tt.expctedLiteral {
+			t.Fatalf("tests[%d] - literal wrong. expected=%q, got=%q", i, tt.expctedLiteral, tok.Literal)
+		}
+
+	}
+}
